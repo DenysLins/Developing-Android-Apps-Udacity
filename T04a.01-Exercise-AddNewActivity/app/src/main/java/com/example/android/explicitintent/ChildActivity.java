cdb7bed63e7c06765140ead8a98df5c09c663b9d
Denys Lins <denyslins@gmail.com>
2017-05-03 10:49:11 -0300

    Add my T04a.01-Exercise-AddNewActivity solution

---
+++ T04a.01-Exercise-AddNewActivity/app/src/main/java/com/example/android/explicitintent/ChildActivity.java
@@ -0,0 +1,21 @@
+package com.example.android.explicitintent;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+public class ChildActivity extends AppCompatActivity {
+
+    private TextView mTextView;
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_child);
+
+
+        mTextView = (TextView) findViewById(R.id.tv_display);
+
+
+    }
+}
