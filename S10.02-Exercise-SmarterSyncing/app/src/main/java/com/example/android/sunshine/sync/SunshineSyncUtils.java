223869970b89fade6fcccb5fdcd03423eb1a34a1
Denys Lins <denyslins@gmail.com>
2017-08-07 23:17:09 -0300

    Add my S10.02-Exercise-SmarterSyncing solution

--- S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
+++ S10.02-Exercise-SmarterSyncing/app/src/main/java/com/example/android/sunshine/sync/SunshineSyncUtils.java
@@ -15,20 +15,35 @@
  */
 package com.example.android.sunshine.sync;
 
+import android.content.ContentResolver;
 import android.content.Context;
 import android.content.Intent;
+import android.database.Cursor;
+import android.os.AsyncTask;
 import android.support.annotation.NonNull;
+
+import com.example.android.sunshine.data.WeatherContract;
 
 
 public class SunshineSyncUtils {
 
-//  TODO (1) Declare a private static boolean field called sInitialized
+    //  Completed (1) Declare a private static boolean field called sInitialized
+    private static boolean sInitialized = false;
+    private static boolean sEmpty = true;
 
-    //  TODO (2) Create a synchronized public static void method called initialize
-    //  TODO (3) Only execute this method body if sInitialized is false
-    //  TODO (4) If the method body is executed, set sInitialized to true
-    //  TODO (5) Check to see if our weather ContentProvider is empty
-        //  TODO (6) If it is empty or we have a null Cursor, sync the weather now!
+    //  Completed (2) Create a synchronized public static void method called initialize
+    synchronized public static void initialize(Context context) {
+        //  Completed (3) Only execute this method body if sInitialized is false
+        if (!sInitialized) {
+            //  Completed (4) If the method body is executed, set sInitialized to true
+            sInitialized = true;
+            //  Completed (5) Check to see if our weather ContentProvider is empty
+            CheckProviderIsEmpty checkProviderIsEmpty = new CheckProviderIsEmpty();
+            checkProviderIsEmpty.execute(context);
+            //  Completed (6) If it is empty or we have a null Cursor, sync the weather now!
+            if (sEmpty) startImmediateSync(context);
+        };
+    }
 
     /**
      * Helper method to perform a sync immediately using an IntentService for asynchronous
@@ -40,4 +55,32 @@ public class SunshineSyncUtils {
         Intent intentToSyncImmediately = new Intent(context, SunshineSyncIntentService.class);
         context.startService(intentToSyncImmediately);
     }
+
+    private static class CheckProviderIsEmpty extends AsyncTask<Context, Void, Boolean> {
+
+        protected Boolean doInBackground(Context... context) {
+
+            boolean result;
+
+            ContentResolver sunshineContentResolver = context[0].getContentResolver();
+            Cursor cursor = sunshineContentResolver.query(
+                    WeatherContract.WeatherEntry.CONTENT_URI,
+                    null,
+                    null,
+                    null,
+                    null);
+
+            result = (cursor == null || cursor.getCount() == 0);
+
+            if (cursor != null) cursor.close();
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(Boolean aBoolean) {
+            sEmpty = aBoolean;
+        }
+    }
+
 }
